97bf5ff6fd2d261a8c65ea52fff3d4d2244e5f0a
Alfredo Montini <montif1975@gmail.com>
2024-09-15 09:06:41 +0200

    First commit (I2C dispaly and sensor work but the code must be refactored and clean).

---
+++ include/sh1106_i2c.h
@@ -0,0 +1,121 @@
+/* 
+SH1106 OLED DISPLAY DRIVER
+*/
+// Define the size of the display we have attached. This can vary, make sure you
+// have the right size defined or the output will look rather odd!
+// Code has been tested on 128x32 and 128x64 OLED displays
+#define SH1106_HEIGHT               64
+#define SH1106_WIDTH                128 //132
+
+#define SH1106_I2C_ADDR             _u(0x3C)
+
+// 400 is usual, but often these can be overclocked to improve display response.
+// Tested at 1000 on both 32 and 84 pixel height devices and it worked.
+#define SH1106_I2C_CLK              400
+
+#if 0
+#define SH1106_SETLOWCOLUMN             0x00
+#define SH1106_SETHIGHCOLUMN            0x10
+#define SH1106_MEMORYMODE               0x20
+#define SH1106_COLUMNADDR               0x21
+#define SH1106_PAGEADDR                 0x22
+// Scrolling #defines
+#define SH1106_RIGHT_HORIZONTAL_SCROLL                  0x26
+#define SH1106_LEFT_HORIZONTAL_SCROLL                   0x27
+#define SH1106_VERTICAL_AND_RIGHT_HORIZONTAL_SCROLL     0x29
+#define SH1106_VERTICAL_AND_LEFT_HORIZONTAL_SCROLL      0x2A
+#define SH1106_DEACTIVATE_SCROLL                        0x2E
+#define SH1106_ACTIVATE_SCROLL                          0x2F
+
+#define SH1106_SETSTARTLINE             0x40
+#define SH1106_SETCONTRAST              0x81
+#define SH1106_CHARGEPUMP               0x8D
+#define SH1106_SEGREMAP                 0xA0
+#define SH1106_SET_VERTICAL_SCROLL_AREA 0xA3
+#define SH1106_DISPLAYALLON_RESUME      0xA4
+#define SH1106_DISPLAYALLON             0xA5
+#define SH1106_NORMALDISPLAY            0xA6
+#define SH1106_INVERTDISPLAY            0xA7
+#define SH1106_SETMULTIPLEX             0xA8
+#define SH1106_DISPLAYOFF               0xAE
+#define SH1106_DISPLAYON                0xAF
+#define SH1106_COMSCANINC               0xC0
+#define SH1106_COMSCANDEC               0xC8
+#define SH1106_SETDISPLAYOFFSET         0xD3
+#define SH1106_SETDISPLAYCLOCKDIV       0xD5
+#define SH1106_SETPRECHARGE             0xD9
+#define SH1106_SETCOMPINS               0xDA
+#define SH1106_SETVCOMDETECT            0xDB
+
+#define SH1106_EXTERNALVCC              0x1
+#define SH1106_SWITCHCAPVCC             0x2
+#endif
+
+
+// commands (see datasheet)
+#define SH1106_SETLOWCOLUMN         _u(0x00)
+#define SH1106_SETHIGHCOLUMN        _u(0x10)
+#define SH1106_SET_MEM_MODE         _u(0x20)
+#define SH1106_SET_COL_ADDR         _u(0x21)
+//#define SH1106_SET_PAGE_ADDR        _u(0x22)
+#define SH1106_SET_HORIZ_SCROLL     _u(0x26)
+#define SH1106_SET_SCROLL           _u(0x2E)
+
+#define SH1106_SET_DISP_START_LINE  _u(0x40)
+
+#define SH1106_SET_CONTRAST         _u(0x81)
+#define SH1106_SET_CHARGE_PUMP      _u(0x8D)
+
+#define SH1106_SET_SEG_REMAP        _u(0xA0)
+#define SH1106_SET_ENTIRE_ON        _u(0xA4)
+#define SH1106_SET_ALL_ON           _u(0xA5)
+#define SH1106_SET_NORM_DISP        _u(0xA6)
+#define SH1106_SET_INV_DISP         _u(0xA7)
+#define SH1106_SET_MUX_RATIO        _u(0xA8)
+#define SH1106_SET_DISP_OFF         _u(0xAE)
+#define SH1106_SET_DISP_ON          _u(0xAF)
+#define SH1106_SET_PAGE_ADDR        _u(0xB0)
+#define SH1106_SET_COM_OUT_DIR      _u(0xC0)
+#define SH1106_SET_COM_OUT_DIR_FLIP _u(0xC0)
+
+#define SH1106_SET_DISP_OFFSET      _u(0xD3)
+#define SH1106_SET_DISP_CLK_DIV     _u(0xD5)
+#define SH1106_SET_PRECHARGE        _u(0xD9)
+#define SH1106_SET_COM_PIN_CFG      _u(0xDA)
+#define SH1106_SET_VCOM_DESEL       _u(0xDB)
+
+#define SH1106_PAGE_HEIGHT          _u(8)
+#define SH1106_NUM_PAGES            (SH1106_HEIGHT / SH1106_PAGE_HEIGHT)
+#define SH1106_BUF_LEN              (SH1106_NUM_PAGES * SH1106_WIDTH)
+
+#define SH1106_WRITE_MODE           _u(0xFE)
+#define SH1106_READ_MODE            _u(0xFF)
+
+// FONTS
+#define FONT_HIGH_8                 8
+#define FONT_HIGH_16                16
+#define FONT_HIGH_24                24
+
+struct render_area {
+    uint8_t start_col;
+    uint8_t end_col;
+    uint8_t start_page;
+    uint8_t end_page;
+
+    int buflen;
+};
+
+// forward declaration
+void calc_render_area_buflen(struct render_area *area);
+void SH1106_init();
+void SH1106_send_cmd(uint8_t cmd);
+void SH1106_send_cmd_list(uint8_t *buf, int num);
+void SH1106_send_buf(uint8_t buf[], int buflen);
+void SH1106_scroll(bool on);
+void render(uint8_t *buf, struct render_area *area);
+//static void SetPixel(uint8_t *buf, int x,int y, bool on);
+//static void DrawLine(uint8_t *buf, int x0, int y0, int x1, int y1, bool on);
+//static int GetFontIndex(uint8_t ch);
+//static void WriteChar(uint8_t *buf, int16_t x, int16_t y, uint8_t ch);
+void WriteString(uint8_t *buf, int16_t x, int16_t y, char *str);
+int SH1106_write_string(uint8_t *buf, int16_t x, int16_t y, char *str, uint8_t font_l, uint8_t font_h);
---
+++ lightThermo.c
@@ -0,0 +1,331 @@
+#include <dht.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "pico/binary_info.h"
+
+#include "include/sh1106_i2c.h"
+
+// change this to match your setup
+static const dht_model_t DHT_MODEL = DHT11;
+static const uint DATA_PIN = 15;
+
+// I2C defines
+// This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define I2C_PORT_OLED           i2c0
+#define I2C_SDA_OLED            8
+#define I2C_SCL_OLED            9
+
+#define I2C_PORT_SENS           i2c1
+#define I2C_SDA_SENS            14
+#define I2C_SCL_SENS            15
+
+
+#if 0
+#include "blink.pio.h"
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+}
+#endif
+
+static float celsius_to_fahrenheit(float temperature) {
+    return temperature * (9.0f / 5) + 32;
+}
+
+void setup_display_layout(uint8_t *buf, int fb_size)
+{
+    int i;
+
+    // build the entire display layout (status bar + info area + extra info area)
+    // __________________
+    // |_____|_____|____|   2 pages - Status Bar (status + ID + Connection state)
+    // |                |   4 pages - Info Area
+    // |________________|
+    // |________________|   2 pages - extra info area
+    //  
+    memset(buf, 0, fb_size);
+    // build vertical line between status and ID field
+    buf[48] = 0xFF; 
+    buf[96] = 0xFF;  
+    // build orizontal line between Status bar and Info area
+    for (i=128; i<256; i++)
+        buf[i] = 0x80;
+    buf[176] = 0xFF;
+    buf[224] = 0xFF;
+    // build orizontal line between Info area and extra info bar
+    for (i=768; i<896; i++)
+        buf[i] = 0x01;
+
+    return;
+}
+
+int main()
+{
+    uint8_t fb[SH1106_BUF_LEN];
+
+    stdio_init_all();
+
+    // wait to give time to hardware to be ready
+    sleep_ms(2000);
+
+#if !defined(I2C_PORT_OLED) || !defined(I2C_SDA_OLED) || !defined(I2C_SCL_OLED)
+#warning i2c / SH1106_i2c example requires a board with I2C pins
+    puts("Define I2C pins were not defined");
+#else
+
+    // useful information for picotool
+    bi_decl(bi_2pins_with_func(I2C_SDA_OLED, I2C_SCL_OLED, GPIO_FUNC_I2C));
+    bi_decl(bi_program_description("SH1106 OLED driver I2C example for the Raspberry Pi Pico"));
+
+    printf("Hello, SH1106 OLED display! Look at my raspberries..\n");
+
+    // I2C for OLED display
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT_OLED, 400*1000);
+    
+    gpio_set_function(I2C_SDA_OLED, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_OLED, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_OLED);
+    gpio_pull_up(I2C_SCL_OLED);
+
+    // I2C for Sensor (Temp, humidity,etc...)
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT_SENS, 400*1000);
+    
+    gpio_set_function(I2C_SDA_SENS, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL_SENS, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA_SENS);
+    gpio_pull_up(I2C_SCL_SENS);
+    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    // run through the complete initialization process
+    // OLED display init
+    SH1106_init();
+
+    // Initialize render area for entire frame (SH1106_WIDTH pixels by SH1106_NUM_PAGES pages)
+    struct render_area frame_area = {
+        start_col: 0,
+        end_col : SH1106_WIDTH - 1,
+        start_page : 0,
+        end_page : SH1106_NUM_PAGES - 1
+        };
+
+    calc_render_area_buflen(&frame_area);
+
+    setup_display_layout(fb,SH1106_BUF_LEN);
+    render(fb, &frame_area);
+
+    SH1106_send_cmd(SH1106_SET_ENTIRE_ON); // go back to following RAM for pixel state
+
+    // only for test
+    char *status_string = "OK";
+    char *id_string = "Sala";
+    char *connection_string ="No";
+    char *test_string = "1234";
+
+#if 0
+    WriteString(fb, 2, 0, status_string);
+    WriteString(fb, 50, 0, id_string);
+    WriteString(fb, 98, 0, connection_string);
+#endif
+
+    SH1106_write_string(fb,2,0,status_string,8,8);
+    SH1106_write_string(fb,50,0,id_string,8,8);
+    SH1106_write_string(fb,98,0,connection_string,8,8);
+
+    SH1106_write_string(fb,0,16,test_string,16,16);
+    SH1106_write_string(fb,0,32,test_string,12,16);
+
+    render(fb, &frame_area);
+
+    // Sensor init (da sistemare)
+    #define DHT20_I2C_ADDRESS       0x38
+    #define DHT20_STATUS_OK         0x18
+    #define DHT20_READ              0x01
+    #define DHT20_WRITE             0x00
+    #define DHT20_START_MEASURE     0xAC
+    int temperature = 0;
+    int humidity = 0;
+    float temp, hum;
+    uint8_t buf[6] = {DHT20_READ,0,0,0,0,0};
+    i2c_write_blocking(I2C_PORT_SENS,DHT20_I2C_ADDRESS,buf,1,false);
+    memset(buf,0x0,sizeof(buf));
+    sleep_ms(100);
+    i2c_read_blocking(I2C_PORT_SENS,DHT20_I2C_ADDRESS,buf,1,false);
+    printf("--> from sensor: buf[0] = %02x\n",buf[0]);
+    if(buf[0] == DHT20_STATUS_OK)
+    {
+        printf("Sensor DHT20 calibrated and ready.\n");
+        uint8_t loop;
+        do {
+            sleep_ms(1000);
+            memset(buf,0x0,sizeof(buf));
+            buf[0] = DHT20_START_MEASURE;
+            buf[1] = 0x33;
+            buf[2] = 0x00;
+            i2c_write_blocking(I2C_PORT_SENS,DHT20_I2C_ADDRESS,buf,3,false);
+            // modificare e leggere direttamente 6 bytes con il primo che e' lo status
+            loop = 5;
+            do {
+                memset(buf,0x0,sizeof(buf));
+                sleep_ms(80);
+                i2c_read_blocking(I2C_PORT_SENS,DHT20_I2C_ADDRESS,buf,6,false);
+                printf("--> loop = %d, from sensor: buf[0] = %02x\n",loop,buf[0]);
+                loop--;
+            } while (((buf[0] & 0x80) == 0x80) && (loop > 0));
+            // measure is ready
+            i2c_read_blocking(I2C_PORT_SENS,DHT20_I2C_ADDRESS,buf,6,false);
+            printf("--> from sensor: buf[] = %02x,%02x,%02x,%02x,%02x\n",buf[1],buf[2],buf[3],buf[4],buf[5]);
+            humidity = (buf[1]<<8) | ((buf[2]));
+            humidity = ((humidity << 4) | ((buf[3] & 0xF0)>>4));
+            printf("humidity = 0x%X\n",humidity);
+            temperature = ((buf[3] & 0x0F)<< 16) | (buf[4]<<8) | buf[5];
+            printf("temperature = 0x%X\n",temperature);
+            hum = ((float)humidity / 1048576)*100;
+            temp = (((float)temperature / 1048576)*200)-50;
+            printf("*** TEMPERATURE = %.2f C\n",temp);
+            printf("*** HUMIDITY = %.2f %%RH\n",hum);
+        } while(true);
+    }
+    else
+    {
+        printf("Sensor DHT20 NOT calibrated!\n");
+        /* todo */
+        do
+        {
+            sleep_ms(1000);
+            printf("Need calibration... :-(\n");
+        } while (true);
+        
+    }
+
+#if 0
+    // intro sequence: flash the screen 3 times
+    for (int i = 0; i < 3; i++) {
+        SH1106_send_cmd(SH1106_SET_ALL_ON);    // Set all pixels on
+        sleep_ms(500);
+        SH1106_send_cmd(SH1106_SET_ENTIRE_ON); // go back to following RAM for pixel state
+        sleep_ms(500);
+    }
+    // render 3 cute little raspberries
+    struct render_area area = {
+        start_page : 0,
+        end_page : (IMG_HEIGHT / SH1106_PAGE_HEIGHT)  - 1
+    };
+
+restart:
+
+    area.start_col = 0;
+    area.end_col = IMG_WIDTH - 1;
+
+    calc_render_area_buflen(&area);
+
+    uint8_t offset = 5 + IMG_WIDTH; // 5px padding
+
+    for (int i = 0; i < 3; i++) {
+        render(raspberry26x32, &area);
+        area.start_col += offset;
+        area.end_col += offset;
+    }
+
+    SH1106_scroll(true);
+    sleep_ms(5000);
+    SH1106_scroll(false);
+
+    char *text[] = {
+        "A long time ago",
+        "  on an OLED ",
+        "   display",
+        " far far away",
+        "Lived a small",
+        "red raspberry",
+        "by the name of",
+        "    PICO"
+    };
+
+    int y = 0;
+    for (uint i = 0 ;i < count_of(text); i++) {
+        WriteString(buf, 5, y, text[i]);
+        y+=8;
+    }
+    render(buf, &frame_area);
+
+    // Test the display invert function
+    sleep_ms(3000);
+    SH1106_send_cmd(SH1106_SET_INV_DISP);
+    sleep_ms(3000);
+    SH1106_send_cmd(SH1106_SET_NORM_DISP);
+
+    bool pix = true;
+    for (int i = 0; i < 2;i++) {
+        for (int x = 0;x < SH1106_WIDTH;x++) {
+            DrawLine(buf, x, 0,  SH1106_WIDTH - 1 - x, SH1106_HEIGHT - 1, pix);
+            render(buf, &frame_area);
+        }
+
+        for (int y = SH1106_HEIGHT-1; y >= 0 ;y--) {
+            DrawLine(buf, 0, y, SH1106_WIDTH - 1, SH1106_HEIGHT - 1 - y, pix);
+            render(buf, &frame_area);
+        }
+        pix = false;
+    }
+
+    goto restart;
+#endif // if 0
+
+#endif
+
+#if 0
+    // PIO Blinking example
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &blink_program);
+    printf("Loaded program at %d\n", offset);
+    
+    #ifdef PICO_DEFAULT_LED_PIN
+    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    #else
+    blink_pin_forever(pio, 0, offset, 6, 3);
+    #endif
+    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    while (true) {
+        printf("Hello, world!\n");
+        sleep_ms(1000);
+    }
+
+    dht_t dht;
+    dht_init(&dht, DHT_MODEL, pio0, DATA_PIN, false /* pull_up */);
+    do {
+        if(dht_start_measurement(&dht) == 0)
+            printf("DHT measure start...");
+        
+        float humidity;
+        float temperature_c;
+        dht_result_t result = dht_finish_measurement_blocking(&dht, &humidity, &temperature_c);
+        if (result == DHT_RESULT_OK) {
+            printf("%.1f C (%.1f F), %.1f%% humidity\n", temperature_c, celsius_to_fahrenheit(temperature_c), humidity);
+        } else if (result == DHT_RESULT_TIMEOUT) {
+            puts("DHT sensor not responding. Please check your wiring.");
+        } else {
+            assert(result == DHT_RESULT_BAD_CHECKSUM);
+            puts("Bad checksum");
+        }
+
+        sleep_ms(2000);
+    } while (true);
+#endif
+
+}
